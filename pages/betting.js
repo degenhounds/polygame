8d0b9b0ece09dc6a9e5d623aa63c039279cda8d9
Full Stack Developer <87520906+yuri0105@users.noreply.github.com>
2022-05-24 19:47:53 +1000

    Update betting.js

--- pages/betting.js
+++ pages/betting.js
@@ -97,7 +97,7 @@ const Betting = ({ wallet, web3, contract, contractAddress, balance, setBalance 
                 }
 
                 setLoading(2);
-            }, 70000);
+            }, 60000);
 
             // contract.events.Result({}, async (error, event) => {
             // // contract.once('Result', {}, async (error, event) => {
