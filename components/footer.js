230d0d433e699907e2c7b8e050dfa7d4a9b67387
degenhounds <105652850+degenhounds@users.noreply.github.com>
2022-11-05 22:22:15 +1030

    Update footer.js

--- components/footer.js
+++ components/footer.js
@@ -3,7 +3,11 @@ import { Box } from '@chakra-ui/react'
 const Footer = () => {
   return (
     <Box align="center" opacity={0.4} fontSize="sm">
-      &copy; {new Date().getFullYear()} Degen Hounds. All Rights Reserved.
+      &copy; {new Date().getFullYear()} Degen Hounds. All Rights Reserved. Special thanks to {" "}
+            <Link href="https://webchain.sellix.io/" target="_blank">
+              Webchain Development!
+        </Link>
+            {" "}
     </Box>
   )
 }
